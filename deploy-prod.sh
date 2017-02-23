6ffda5069d4d3c2890b8271e8c12f12c4efbecf1
Christopher Burroughs <chris@vps377821.ovh.net>
2017-02-23 18:58:42 +0100

    fix typos

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Launches a development instance using Gunicorn on port 80
+# Launches a production instance using Gunicorn on port 81
 # Reads from docker-compose.yml only
 
 PROJECT="botapadd-prod"
 
 #docker-compose -f docker-compose.yml pull
-docker-compose -f docker-compose.yml up --no-build force-recreate -d
+docker-compose -f docker-compose.yml up --no-build --force-recreate -d
 docker-compose logs -ft
