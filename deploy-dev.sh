c752689f946f1ff2dffff66ebe1be666d7a4b537
ynnk <ychudy@gmail.com>
2018-01-14 15:50:55 +0100

    rm  docker

--- deploy-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Launches a development instance using Flask on port 5000
-# Reads from docker-compose.yml + docker-compose.override.yml
-
-PROJECT="botapadd-dev"
-
-docker-compose up --build --force-recreate -d
-docker-compose logs -ft
